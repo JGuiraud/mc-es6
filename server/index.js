d8ea1bca0f803b5c8157f505c31d51762420c9dc
JGuiraud <jerome31guiraud@gmail.com>
2017-03-13 22:21:23 +0100

    post not working yet

--- server/index.js
+++ server/index.js
@@ -1,1 +1,52 @@
 //to do server
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path'
+import fs from 'fs';
+
+const urlJSON = '/database/tasks.json'
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+})
+
+app.get("/", (req, res) => {
+    res.redirect("/home")
+})
+
+app.post('/addnew', (req, res) => {
+    console.log("yo")
+    fs.readFile('/database/tasks.json', 'utf-8', (err, res) => {
+        if (err) {
+            throw err
+            console.log(err)
+        };
+        let database = JSON.parse(res);
+        let len = database.taches.length;
+
+        let newTodo = req.body;
+
+        newTodo.id = (len + 1).toString();
+        database.taches.push(req.body);
+
+        let newDatabase = JSON.stringify(database, null, 2)
+
+        fs.writeFile("/database/tasks.json", newDatabase, (err) => {
+            if (err) {
+                throw err
+            }
+        });
+    });
+});
+
+app.get('/data', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', urlJSON));
+})
+
+app.listen(8000, () => console.log('LISTEN ON PORT 8000'));
--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,5 +1,27 @@
 (() => {
   const app = {
-    //todo
-  }
+    dataTodo: null,
+    init() {
+      console.log("ready !")
+      this.getTasks();
+    },
+    getTasks() {
+      fetch('/data')
+        // fetch('../database/tasks.json')
+        .then((res) => res.json())
+        .then((data) => {
+          this.dataTodo = data.taches;
+          this.tasks()
+        });
+    },
+    tasks() {
+      this.dataTodo.map(this.lihtml)
+    },
+    lihtml(taches) {
+      document.getElementById("todoUl").innerHTML += `<li id=${taches.id}>${taches.content}</li>`
+    }
+  };
+  app.init()
 })();
+
+// npm run server
